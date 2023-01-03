78d4fce705900bd7fd14b7bc6ea09e8606491048
Kristof Varga <kristof.varga@viesure.io>
2023-01-03 16:32:48 +0100

    FeedLoader

---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,11 @@
+//
+//  Copyright © 2023. Viesure. All rights reserved.
+//
+
+import Foundation
+
+typealias LoadFeedResult = Result<[FeedItem], Error>
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
