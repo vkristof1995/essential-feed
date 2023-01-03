97996f1f3ec638f895fcfa491bb66c2cf7dd8bfb
Kristof Varga <kristof.varga@viesure.io>
2023-01-03 16:30:42 +0100

    create FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright © 2023. Viesure. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
