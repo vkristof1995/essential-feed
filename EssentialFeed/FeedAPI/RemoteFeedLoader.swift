74c3af629bdc4c8532f29efecf66f88e15471fe8
Kristof Varga <kristof.varga@viesure.io>
2023-01-03 18:23:45 +0100

    move RemoteFeedLoader to the framework

---
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -0,0 +1,24 @@
+//
+//  Copyright © 2023. Viesure. All rights reserved.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public class RemoteFeedLoader {
+    
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: URL(string: "http://google.com")!)
+    }
+}
--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -3,25 +3,7 @@
 //
 
 import XCTest
-
-class RemoteFeedLoader {
-    
-    let url: URL
-    let client: HTTPClient
-    
-    init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    func load() {
-        client.get(from: URL(string: "http://google.com")!)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
+import EssentialFeed
 
 class RemoteFeedLoaderTests: XCTestCase {
 
