1e8b522d4b388d34fb816e6d27db1bbfb00b044b
Kristof Varga <kristof.varga@viesure.io>
2023-01-04 16:39:45 +0100

    combine spy capture params into a single tuple

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -59,16 +59,19 @@ class RemoteFeedLoaderTests: XCTestCase {
         var requestedURL: URL? {
             requestedURLs.last
         }
-        var requestedURLs: [URL] = []
-        var completions = [(Error) -> Void]()
+        
+        var requestedURLs: [URL] {
+            messages.map { $0.url }
+        }
+        
+        private var messages: [(url: URL, completion: (Error) -> Void)] = []
         
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            completions.append(completion)
-            requestedURLs.append(url)
+            messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            completions[index](error)
+            messages[index].completion(error)
         }
     }
 }
