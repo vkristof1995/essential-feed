e6e3b98cd6c6198bfab971e25ca5e1ddbcce53d9
Kristof Varga <kristof.varga@viesure.io>
2023-01-04 16:48:43 +0100

    add more tests for http status codes

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -49,12 +49,20 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
         
-        var capturedErrors = [RemoteFeedLoader.Error]()
-        sut.load { capturedErrors.append($0) }
         
-        client.complete(withStatusCode: 200)
         
-        XCTAssertEqual(capturedErrors, [.invalidData])
+        [199, 201, 300, 400, 500].enumerated().forEach { index, code in
+            
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load { capturedErrors.append($0) }
+            
+            client.complete(withStatusCode: code, at: index)
+            
+            XCTAssertEqual(capturedErrors, [.invalidData])
+            
+
+        }
+        
     }
     
     // MARK: - Helper
