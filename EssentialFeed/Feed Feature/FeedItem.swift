3e40ea72b924b318d8059e6e5e46913859c06119
Kristof Varga <kristof.varga@viesure.io>
2023-01-11 18:17:37 +0100

    move coddle logic to new api item model

--- EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -4,18 +4,11 @@
 
 import Foundation
 
-public struct FeedItem: Equatable, Codable {
+public struct FeedItem: Equatable {
     public let id: UUID
     public let description: String?
     public let location: String?
     public let imageURL: URL
-    
-    enum CodingKeys: String, CodingKey {
-        case id
-        case description
-        case location
-        case imageURL = "image"
-    }
     
     public init(id: UUID, description: String?, location: String?, imageURL: URL) {
         self.id = id
--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -29,9 +29,9 @@ public class RemoteFeedLoader {
     public func load(completion: @escaping (RemoteFeedLoaderResult) -> Void) {
         client.get(from: url) { result in
             switch result {
-            case let .success(data, response):
+            case .success(let (data, response)):
                 if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items))
+                    completion(.success(root.items.map { $0.item }))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -42,6 +42,17 @@ public class RemoteFeedLoader {
     }
 }
 
-private struct Root: Codable {
-    let items: [FeedItem]
+private struct Root: Decodable {
+    let items: [Item]
+}
+
+private struct Item: Decodable {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL
+    
+    var item: FeedItem {
+        FeedItem(id: id, description: description, location: location, imageURL: image)
+    }
 }
