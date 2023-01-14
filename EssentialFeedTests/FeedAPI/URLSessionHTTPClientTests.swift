9cbed154dc0cf66b5074e3d53ef5d497653e90c8
Kristof Varga <kristof.varga@viesure.io>
2023-01-14 18:42:30 +0100

    create URLSessionDatatask with urls

---
+++ EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -0,0 +1,48 @@
+//
+//  Copyright © 2023. Viesure. All rights reserved.
+//
+
+import XCTest
+
+class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) { _, _, _ in
+            
+        }
+    }
+}
+
+class URLSEssionHTTPClientTests: XCTestCase {
+    
+    func test() {
+        let url = URL(string: "https://google.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+    
+    // MARK: - HElpers
+    
+    private class URLSessionSpy: URLSession {
+        var receivedURLs: [URL] = []
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            
+            return FakeURLSessionDataTask()
+        }
+    }
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask {
+        
+    }
+}
