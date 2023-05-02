b2b5b954e33c38ecf5387ae4818dd1c57d3c3e41
Kristof Varga <kristof.varga@viesure.io>
2023-05-02 20:12:55 +0200

    extract cache insertion into a helper function

--- EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -19,12 +19,16 @@ class LocalFeedLoader {
             if let cachedDeletionError = error {
                 completion(cachedDeletionError)
             } else {
-                self.store.insert(items, timestamp: self.currentDate(), completion: { [weak self] error in
-                    guard self != nil else { return }
-                    completion(error)
-                })
+                self.cache(items, with: completion)
             }
         }
+    }
+    
+    private func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+        store.insert(items, timestamp: currentDate(), completion: { [weak self] error in
+            guard self != nil else { return }
+            completion(error)
+        })
     }
 }
 
