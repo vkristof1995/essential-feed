76eb407bcbaafabdc9fda0e27c5b82b7d373bfb5
Kristof Varga <kristof.varga@viesure.io>
2023-05-26 13:52:59 +0200

    dispatch operations to serial background queue

--- EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -207,7 +207,7 @@ class FeedStoreTests: XCTestCase {
             deletionError = receivedDeletionError
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 1.0)
+        wait(for: [exp], timeout: 2.0)
         return deletionError
     }
     
