667de0512d85754b4dc846be86d1801c5c4d2e27
Kristof Varga <kristof.varga@viesure.io>
2023-05-26 14:05:18 +0200

    add comments

--- EssentialFeed/FeedCache/FeedStore.swift
+++ EssentialFeed/FeedCache/FeedStore.swift
@@ -22,5 +22,8 @@ public protocol FeedStore {
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch to appropiate threads, if needed
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropiate threads, if needed
     func retrieve(completion: @escaping RetrievalCompletion)
 }
