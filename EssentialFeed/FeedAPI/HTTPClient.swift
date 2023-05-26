65fb88bc8ed2dfe2ec0214fb53a942f0da6c5d81
Kristof Varga <kristof.varga@viesure.io>
2023-05-26 14:05:03 +0200

    add comments

--- EssentialFeed/FeedAPI/HTTPClient.swift
+++ EssentialFeed/FeedAPI/HTTPClient.swift
@@ -7,5 +7,8 @@ import Foundation
 public typealias HttpClientResult = Result<(Data, HTTPURLResponse), Error>
 
 public protocol HTTPClient {
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropiate threads, if needed
     func get(from url: URL, completion: @escaping (HttpClientResult) -> Void)
 }
