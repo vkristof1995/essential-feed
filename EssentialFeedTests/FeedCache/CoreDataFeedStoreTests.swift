543705549ad91f14ffef42606018e249d1540ca0
Kristof Varga <kristof.varga@viesure.io>
2023-10-30 09:55:16 +0100

    test_insert_overridesPreviouslyInsertedCacheValues

--- EssentialFeedTests/FeedCache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CoreDataFeedStoreTests.swift
@@ -43,9 +43,11 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
         
         assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
     }
-
+    
     func test_insert_overridesPreviouslyInsertedCacheValues() {
-
+        let sut = makeSUT()
+        
+        assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
     }
 
     func test_delete_deliversNoErrorOnEmptyCache() {
