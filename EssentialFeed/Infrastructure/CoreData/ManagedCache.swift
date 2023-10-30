b54f40c1c78059df962caf1d7950de599070e6e3
Kristof Varga <kristof.varga@viesure.io>
2023-10-30 10:31:06 +0100

    move things to extension

--- EssentialFeed/Infrastructure/CoreData/ManagedCache.swift
+++ EssentialFeed/Infrastructure/CoreData/ManagedCache.swift
@@ -9,6 +9,10 @@ class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
     @NSManaged var feed: NSOrderedSet
     
+}
+
+extension ManagedCache {
+    
     static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
         let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
         request.returnsObjectsAsFaults = false
--- EssentialFeed/Infrastructure/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -12,6 +12,10 @@ class ManagedFeedImage: NSManagedObject {
     @NSManaged var url: URL
     @NSManaged var cache: ManagedCache
     
+}
+
+extension ManagedFeedImage{
+    
     static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
         return NSOrderedSet(array: localFeed.map { local in
             let managed = ManagedFeedImage(context: context)
