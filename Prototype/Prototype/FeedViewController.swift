72559f42ecbb4344fc517e22e3490ae92ae36c0a
Kristof Varga <kristof.varga@viesure.io>
2023-10-30 15:23:44 +0100

    Add FeedImageCell with storyboard connections

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -11,29 +11,13 @@ struct FeedImageViewModel {
 }
 
 final class FeedViewController: UITableViewController {
-    private let feed = FeedImageViewModel.prototypeFeed
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return feed.count
+        return 10
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath) as! FeedImageCell
-        let model = feed[indexPath.row]
-        cell.configure(with: model)
-        return cell
+        return tableView.dequeueReusableCell(withIdentifier: "FeedImageCell")!
     }
-
-}
-
-extension FeedImageCell {
-    func configure(with model: FeedImageViewModel) {
-        locationLabel.text = model.location
-        locationContainer.isHidden = model.location == nil
-
-        descriptionLabel.text = model.description
-        descriptionLabel.isHidden = model.description == nil
-
-        fadeIn(UIImage(named: model.imageName))
-    }
+    
 }
