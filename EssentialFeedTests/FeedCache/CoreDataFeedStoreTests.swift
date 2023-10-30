38a9eeb1ad8186ee42f16b2450de5b7c00db584e
Kristof Varga <kristof.varga@viesure.io>
2023-10-30 09:56:28 +0100

    test_delete_deliversNoErrorOnEmptyCache

--- EssentialFeedTests/FeedCache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CoreDataFeedStoreTests.swift
@@ -49,9 +49,11 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
         
         assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
     }
-
+    
     func test_delete_deliversNoErrorOnEmptyCache() {
-
+        let sut = makeSUT()
+        
+        assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
     }
 
     func test_delete_hasNoSideEffectsOnEmptyCache() {
