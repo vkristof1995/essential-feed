90f6d350ab8bd9ae4fbeef3cd9bba6cabb8bcf78
Kristof Varga <kristof.varga@viesure.io>
2023-10-30 09:41:17 +0100

    ManagedFeedImage, ManagedCache

--- EssentialFeed/FeedCache/CoreDataFeedStore.swift
+++ EssentialFeed/FeedCache/CoreDataFeedStore.swift
@@ -3,6 +3,7 @@
 //
 
 import Foundation
+import CoreData
 
 public final class CoreDataFeedStore: FeedStore {
     public init() {}
@@ -19,4 +20,17 @@ public final class CoreDataFeedStore: FeedStore {
 
     }
 
+}
+
+private class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+}
+
+private class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
 }
