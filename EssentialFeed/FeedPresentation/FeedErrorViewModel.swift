e369740322299bdefa435f490e8b486cea33f751
Kristof Varga <kristof.varga@viesure.io>
2023-11-03 16:18:52 +0100

    move viewmodels to new files

---
+++ EssentialFeed/FeedPresentation/FeedErrorViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  Copyright © 2023 Viesure. All rights reserved.
+//
+
+public struct FeedErrorViewModel {
+    public let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+    
+    static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
+}
---
+++ EssentialFeed/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  Copyright © 2023 Viesure. All rights reserved.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
