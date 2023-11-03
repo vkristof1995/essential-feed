955b5b49595173fa2de3a668094ca5602b3b0184
Kristof Varga <kristof.varga@viesure.io>
2023-11-03 14:21:32 +0100

    Move title configuration to composer

--- EssentialFeediOS/FeedUI/Controllers/FeedUIComposer.swift
+++ EssentialFeediOS/FeedUI/Controllers/FeedUIComposer.swift
@@ -16,6 +16,7 @@ public final class FeedUIComposer {
         let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
         
         feedController.delegate = presentationAdapter
+        feedController.title = FeedPresenter.title
         
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader),
