6713644647de91483bda3331edce6d5b050a8fbf
Kristof Varga <kristof.varga@viesure.io>
2023-11-06 14:59:32 +0100

    FeedLoaderWithFallbackComposite.load delivers primary feed on primary loader success

---
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,60 @@
+//
+//  Copyright © 2023 Viesure. All rights reserved.
+//
+
+import XCTest
+import EssentialFeed
+
+class FeedLoaderWithFallbackComposite: FeedLoader {
+    private let primary: FeedLoader
+
+    init(primary: FeedLoader, fallback: FeedLoader) {
+        self.primary = primary
+    }
+
+    func load(completion: @escaping (LoadFeedResult) -> Void) {
+        primary.load(completion: completion)
+    }
+}
+
+class FeedLoaderWithFallbackCompositeTests: XCTestCase {
+
+    func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
+        let primaryFeed = uniqueFeed()
+        let fallbackFeed = uniqueFeed()
+        let primaryLoader = LoaderStub(result: .success(primaryFeed))
+        let fallbackLoader = LoaderStub(result: .success(fallbackFeed))
+        let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+            case let .success(receivedFeed):
+                XCTAssertEqual(receivedFeed, primaryFeed)
+
+            case .failure:
+                XCTFail("Expected successful load feed result, got \(result) instead")
+            }
+
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+
+    private func uniqueFeed() -> [FeedImage] {
+        return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "http://any-url.com")!)]
+    }
+
+    private class LoaderStub: FeedLoader {
+        private let result: LoadFeedResult
+
+        init(result: LoadFeedResult) {
+            self.result = result
+        }
+
+        func load(completion: @escaping (LoadFeedResult) -> Void) {
+            completion(result)
+        }
+    }
+
+}
