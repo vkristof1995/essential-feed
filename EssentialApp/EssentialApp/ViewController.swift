ef80215a45a798651bb802acda2bcc9050cea4e7
Kristof Varga <kristof.varga@viesure.io>
2023-11-06 14:07:57 +0100

    create empty app project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright © 2023 Viesure. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
