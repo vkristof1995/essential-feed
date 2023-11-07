6ef889a0965b23e30ce24b86fc747092d8218687
Kristof Varga <kristof.varga@viesure.io>
2023-11-07 15:54:26 +0100

    Move FeedCache to the Feed Feature production group

---
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedCache.swift
@@ -0,0 +1,11 @@
+//
+//  Copyright © 2023 Viesure. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedCache {
+    typealias Result = Swift.Result<Void, Error>
+
+    func save(_ feed: [FeedImage], completion: @escaping (Result) -> Void)
+}
